ad09f0f66700336ae0d70f7ed898556ae02589a6
MassMurderer <jure.bagic@gmail.com>
2022-11-17 16:34:50 +0100

    Add readme_resources dir, vga module update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rust-os
-OS made in Rust that you can learn to also build from here https://os.phil-opp.com/.
+OS made in Rust that you can learn to build from https://os.phil-opp.com/.
+
+## Vga_buffer
+
+Valid ASCII:
+[Code page 437](readme_resources\437.png)
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -58,3 +58,48 @@ const BUFFER_WIDTH: usize = 80;
 struct Buffer {
     chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
+
+struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            b => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line()
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: b,
+                    color_code: self.color_code
+                };
+
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) { 
+        self.column_position = 0;
+    }
+}
+
+// pub API
+impl Writer {
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+}
