5f54705467f93f4928f36c8f09f23e7c9ccfd77e
MassMurderer <jure.bagic@gmail.com>
2022-11-21 23:42:40 +0100

    Update should_panic.rs integration test

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,8 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use nostd_color::{colorize::Colored, colors::{BRIGHT_GREEN, BRIGHT_RED, YELLOW}};
@@ -10,12 +7,13 @@ use rust_os::{exit_qemu, QemuExitCode, serial_print, serial_println};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[{}]", "failed".fg(BRIGHT_RED));
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
-// If test panics, then test passes. Only one test can be defined, because after each panic, 
-// we exit qemu.
+// If test panics, then test passes.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     serial_println!("[{}]", "ok".fg(BRIGHT_GREEN));
@@ -23,16 +21,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    for test in tests {
-        test();
-        serial_print!("[{}]", "failed".fg(BRIGHT_RED));
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
 fn should_fail() {
     serial_print!("{}...\t", "should_panic::should_fail".fg(YELLOW));
     assert!(1 == 0);
